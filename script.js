ae743dab57c5b7f6d5ba0858d6aff0133693c68b
JGanseij <88677380+JGanseij@users.noreply.github.com>
2021-09-27 22:12:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-console.log("Hello Winc Academy");
-let first_name = "Jeroen";
-console.log(first_name);
+console.log("Hallo Winc Academy Studenten")
 
-let number2 = 2 + 2;
-let number_2 = "2+2";
+// Dit is een grote som
 
-console.log(number2);
-console.log(number_2);
-
-let sum = 1 + 1;
-let devide = 4 / 1;
-let subtraction = 5 - 4;
-let multiplication = 6 * 6;
-let modulus = 12 % 5;
-
-console.log(sum);
-console.log(devide);
-console.log(subtraction);
-console.log(multiplication);
-console.log(modulus);
-
-let age = "achtentwintig";
-
-console.log(typeof age);
+/* console.log(1230941 * 1823794 )
+console.log(637263/54) */
